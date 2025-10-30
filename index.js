0c11cc46d0a9a43cd7b88d6e99d9f89b204ae7f6
Sahan Wijesinghe <sahanwijesinghe27@gmail.com>
2025-10-30 23:25:15 +0530

    sum

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import reviewRouter from "./routes/reviewRouter.js";
 import inquiryRouter from "./routes/inquiryRouter.js";
 import cors from "cors";
 import orderRouter from "./routes/orderRouter.js";
+import studentRouter from "./routes/studentRoutes.js";
 
 dotenv.config();
 
@@ -51,7 +52,7 @@ app.use("/api/products", productRouter);
 app.use("/api/reviews", reviewRouter);
 app.use("/api/inquiries", inquiryRouter);
 app.use("/api/orders",orderRouter)
-
+app.use("/api/students", studentRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
